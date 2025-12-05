2ab12e506e2ba8a19545d213b4de68193ecbadd1
추호성 <hosung@chuhoseong-ui-MacBookPro.local>
2025-12-05 21:48:43 +0900

    INIT

---
+++ src/test/kotlin/fanteract/account/AccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package fanteract.account
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
